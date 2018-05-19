420559ba9030e2a753907f17a61412417b0701d5
Frances Lois Ritualo <ffritualo@up.edu.ph>
2018-05-20 03:20:51 +0800

    signin

--- views/controllers/header-ctrl.js
+++ views/controllers/header-ctrl.js
@@ -9,10 +9,26 @@
 
     function NavCtrl ($rootScope, $scope, $state, $stateParams){
         var vm             = this;
-
+        $scope.curr =0;
       
+        vm.data = [];
+        vm.data[0] = {
+        	'type':  'Rock Slide',
+        	'place' : 'Jamboree Rd, Los Banos, Laguna',
+        	'date' : 1526755381,
+        	'description': 'A landslide caused by rock failure in which part of the bedding plane of failure passes through intact rock.'
+        }
+        vm.data[1] = {
+        	'type':  'Earth Flow',
+        	'place' : 'Jamboree Rd, Los Banos, Laguna',
+        	'date' : 1526755381,
+        	'description': 'A specific type of Soil Flow landslide where the majority of the soil materials are fine-grained (silt and clay) and cohesive.'
+        }
 
+        function add(){
+        		$scope.curr+=1;
+        		console.log($scope.curr);
 
-
+        }
     }
 })();
