0314177e5dbddb9f05ad570ef468df9d197cf4ba
KennethLloyd <kpverastigue@up.edu.ph>
2018-05-20 18:27:14 +0800

    fworking dummy data

--- views/controllers/header-ctrl.js
+++ views/controllers/header-ctrl.js
@@ -13,16 +13,25 @@
       
         vm.data = [];
         vm.data[0] = {
+            'img_path': '../images/rockslide.jpg',
         	'type':  'Rock Slide',
         	'place' : 'Jamboree Rd, Los Banos, Laguna',
-        	'date' : 1526755381,
+        	'date' : 'May 21, 2018 10:00 AM PST',
         	'description': 'A landslide caused by rock failure in which part of the bedding plane of failure passes through intact rock.'
         }
         vm.data[1] = {
+            'img_path': '../images/earthflow.jpg',
         	'type':  'Earth Flow',
-        	'place' : 'Jamboree Rd, Los Banos, Laguna',
-        	'date' : 1526755381,
-        	'description': 'A specific type of Soil Flow landslide where the majority of the soil materials are fine-grained (silt and clay) and cohesive.'
+        	'place' : 'Mayondon, Los Banos, Laguna',
+        	'date' : 'May 18, 2018 07:00 AM PST',
+        	'description': 'A specific type of Soil Flow landslide where soil materials are fine-grained (silt and clay) and cohesive.'
+        }
+        vm.data[2] = {
+            'img_path': '../images/slump.jpg',
+            'type':  'Slump',
+            'place' : 'Bayog, Los Banos, Laguna',
+            'date' : 'May 12, 2018 08:00 PM PST',
+            'description': 'A form of mass wasting that occurs when rock layers moves a short distance down a slope.'
         }
 
         $rootScope.$on("CallShowMethod", function(index){
@@ -30,14 +39,19 @@
            $scope.showCurr(index);
         });
 
-        $scope.add = () => {
-            if($scope.curr < vm.data.length){
-
+        $scope.next = () => {
+            if($scope.curr < vm.data.length-1){
         		$scope.curr+=1;
             }
-    		console.log($scope.curr);
-
         }
+
+        $scope.prev = () => {
+            if($scope.curr > 0){
+
+                $scope.curr-=1;
+            }
+        }
+
         $scope.showCurr = (current) => {
            $scope.curr = current;
             console.log("curr: " + current);
